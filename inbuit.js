fbc57cd8fc6126c446a7c0468dfb118731bfe9fb
srinath ragalla <srinathragalla17@gmail.com>
2021-12-14 12:11:21 +0530

    inbuit

---
+++ inbuit.js
@@ -0,0 +1,22 @@
+// .tostring()
+var num=5;
+console.log(typeof(num))
+console.log(num.toString())
+console.log(typeof(num.toString()))
+
+//to convert string to number
+var str="9";
+console.log(typeof(str))
+console.log(parseInt(str))
+console.log(typeof(parseInt(str)))
+
+   //parsefloat() --parses a string and returns a floating point number
+var strNum="84.78";
+console.log(strNum);
+console.log(typeof(strNum));
+console.log(parseFloat(strNum));
+  //tofixed() --takes floating point number and rounds the value at a certain point
+  var num= 19.920212022;
+  console.log(num)
+  console.log(num.toFixed(5))
+  console.log(typeof(num))  
